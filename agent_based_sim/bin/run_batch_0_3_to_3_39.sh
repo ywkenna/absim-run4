53a8b74087f08b74beb75600db4ea8975b742fd4
ywkenna <ennakim2422@gmail.com>
2026-01-20 16:30:19 +0900

    a few result added

--- agent_based_sim/bin/run_batch_0_3_to_3_39.sh
+++ agent_based_sim/bin/run_batch_0_3_to_3_39.sh
@@ -5,9 +5,9 @@ LOG_DIR="./logs"
 
 mkdir -p "$LOG_DIR"
 
-for state in {0..3}
+for state in {1..3}
 do
-  for policy in {3..39}
+  for policy in {0..2}
   do
     CFG="$DATA_DIR/2026shelterseek_${state}_${policy}_mpdua_event.json"
 
